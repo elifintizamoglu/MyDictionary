9aee8638734c121c43e7e5c21f401e932524991b
Elif <elifintizam@gmail.com>
2021-02-02 12:08:01 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myDictionary<int ,string> vocabulary = new myDictionary<int ,string>();
+
+            vocabulary.Add(1 , "ÇAY");
+            vocabulary.Add(2 , "KAHVE");
+            vocabulary.Add(3 , "SICAK ÇİKOLATA");
+
+            Console.WriteLine(vocabulary.Length);
+            for(int i=0;i<vocabulary.Length;i++)
+                Console.WriteLine(vocabulary.Items[i] + "     " + (i + 1)  );
+        }
+    }
+}
---
+++ MyDictionary/myDictionary.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class myDictionary<Tkey, Tvalue>
+    {
+        Tkey[] keyArray;
+        Tvalue[] valueArray;
+
+        public myDictionary()
+        {
+            keyArray = new Tkey[0];
+            valueArray = new Tvalue[0];
+        }
+
+        public void Add(Tkey key, Tvalue value)
+        {
+            Tkey[] TempKeyArray = keyArray; //geçiçi bir arraye "array"in adresini atıyoruz
+            keyArray = new Tkey[keyArray.Length + 1];//array'in eleman saysını arttırıyoruz
+
+            Tvalue[] TempValueArray = valueArray;
+            valueArray = new Tvalue[valueArray.Length + 1];
+
+            for(int i=0; i < TempKeyArray.Length; i++)
+            {
+                keyArray[i] = TempKeyArray[i];   //geçici array'deki elemanları yeni oluşturduğumuz ve eleman sayısını arttırdığımız arraye atıyoruz
+                valueArray[i] = TempValueArray[i];
+            }
+            keyArray[keyArray.Length - 1] = key; //yeni array'in son elemanına eklenmek istenen elemanı ekliyoruz.
+            valueArray[valueArray.Length - 1] = value;
+        }
+
+        public int Length
+        {
+            get { return keyArray.Length; }
+        }
+        
+
+        public Tvalue[] Items
+        {
+            get { return valueArray; }
+        }
+
+    }
+}
